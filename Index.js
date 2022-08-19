6a90985e30f785c9a057a43029c46b24df3e7a9d
fernandapairet <fernanda.pairet@yahoo.com.br>
2022-08-19 17:15:02 -0300

    alteracoes de palavras com acento

--- Index.js
+++ Index.js
@@ -2,8 +2,8 @@ import chalk from 'chalk'
 import readlineSync from 'readline-sync'
 
 
-var quem = readlineSync.question('Quem é você?')
-var desejo = readlineSync.question('O que você deseja?')
+var quem = readlineSync.question('Quem e voce?')
+var desejo = readlineSync.question('O que voce deseja?')
 var motivo = readlineSync.question('Para que?')
 console.log('Como ' + (chalk.blue(quem)) + ' eu desejo ' + (chalk.red(desejo)) + ' ' +
 (chalk.green(motivo)))
